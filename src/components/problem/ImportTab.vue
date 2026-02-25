32aee61fbf005bc11efe4706366bbb4d5401ed33
LittleOrange666 <orangeminecraft123@gmail.com>
2026-02-25 21:43:58 +0800

    v0.2.5 roughly done import, versions page

--- src/components/problem/ImportTab.vue
+++ src/components/problem/ImportTab.vue
@@ -1,16 +1,93 @@
 <template>
     <div>
         <h3>匯入與匯出</h3>
+        <div class="card card-body">
+            <h3>匯入Polygon題目</h3>
+            <form @submit.prevent="importPolygon">
+                <div class="mb-3">
+                    <input type="file" class="form-control" accept=".zip" name="zip_file" id="file_input_polygon" required>
+                </div>
+                <div class="mb-3">
+                    <button class="btn btn-primary submitter">
+                        上傳zip檔
+                    </button>
+                </div>
+            </form>
+        </div>
+        <div class="card card-body">
+            <form @submit.prevent="exportStandard">
+                <div class="mb-3">
+                    <button class="btn btn-primary submitter">
+                        匯出題目
+                    </button>
+                </div>
+            </form>
+            <h3>匯入題目</h3>
+            <form @submit.prevent="importStandard">
+                <div class="mb-3">
+                    <input type="file" class="form-control" accept=".zip" name="zip_file" id="file_input_standard" required>
+                </div>
+                <div class="mb-3">
+                    <button class="btn btn-primary submitter">
+                        上傳zip檔
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import {defineProps} from "vue";
 import {ProblemManageDetail} from "@/utils/problemdatas";
+import {api, getParam} from "@/utils/tools";
+import {show_modal} from "@/utils/modal";
+import {useRouter} from "vue-router";
+import axios from "axios";
 
 interface Props{
     data: ProblemManageDetail;
 }
 
 defineProps<Props>();
+const pid = getParam("pid");
+const router = useRouter();
+
+async function importPolygon(){
+    const files = document.getElementById("file_input_polygon") as HTMLInputElement;
+    try{
+        await api.post("/problem/"+pid+"/manage/import/polygon",{
+            zip_file: files.files[0]
+        });
+        await show_modal("成功", "上傳成功");
+    }catch(err){
+        await show_modal("失敗", err.message);
+    }
+}
+
+async function importStandard(){
+    const files = document.getElementById("file_input_standard") as HTMLInputElement;
+    try{
+        await api.post("/problem/"+pid+"/manage/import/standard",{
+            zip_file: files.files[0]
+        });
+        await show_modal("成功", "上傳成功");
+    }catch(err){
+        await show_modal("失敗", err.message);
+    }
+}
+
+async function exportStandard(){
+    const response = await axios.get("/api/problem/"+pid+"/manage/export/standard", {
+        responseType: 'blob'
+    });
+    const href = URL.createObjectURL(response.data);
+    const link = document.createElement('a');
+    link.href = href;
+    link.setAttribute('download', "problem.zip");
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(href);
+}
 </script>
--- src/components/problem/VersionsTab.vue
+++ src/components/problem/VersionsTab.vue
@@ -1,16 +1,64 @@
 <template>
     <div>
         <h3>發布更新</h3>
+        <div class="row g-3">
+            <div class="col-auto">
+                <label for="version_name_input" class="visually-hidden">描述</label>
+                <input type="text" class="form-control" id="version_name_input" placeholder="描述" v-model="name">
+            </div>
+            <div class="col-auto">
+                <button class="btn btn-primary mb-3" id="create_version" v-on:click="createVersion">
+                    發布更新
+                </button>
+            </div>
+        </div>
+        <table class="table table-hover">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">描述</th>
+                <th scope="col">時間</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(version, i) in data.versions" :key="i">
+                <th scope="row">{{ version.id }}</th>
+                <td>{{ version.message }}</td>
+                <td class="date-string">{{ useDateFormat(version.date*1000, 'YYYY/MM/DD HH:mm:ss') }}</td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script setup lang="ts">
-import {defineProps} from "vue";
+import {defineProps, ref} from "vue";
 import {ProblemManageDetail} from "@/utils/problemdatas";
+import {useDateFormat} from "@vueuse/shared";
+import {api, getParam} from "@/utils/tools";
+import {show_modal} from "@/utils/modal";
+import {useRouter} from "vue-router";
 
 interface Props{
     data: ProblemManageDetail;
 }
 
 defineProps<Props>();
+
+const name = ref("");
+
+const router = useRouter();
+const pid = getParam("pid");
+
+async function createVersion(){
+    try {
+        await api.post("/problem/" + pid + "/manage/version", {
+            description: name.value
+        });
+        await show_modal("成功", "建立成功");
+        router.go(0);
+    } catch (err) {
+        await show_modal("失敗", err.message);
+    }
+}
 </script>
