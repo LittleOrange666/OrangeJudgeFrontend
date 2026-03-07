9130640a7057359de8b8dbc7c718ce96a276db09
LittleOrange666 <orangeminecraft123@gmail.com>
2026-03-07 11:52:58 +0800

    add LICENSE

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,17 @@
+/*
+ * Copyright (C) 2024-2026 LittleOrange666 (orangeminecraft123@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ */
+
 import {createRouter, createWebHistory, RouteLocation} from 'vue-router'
 import {Permission, useAuthStore} from "@/stores/auth";
 import {show_modal} from "@/utils/modal";
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,3 +1,17 @@
+<!--
+  - Copyright (C) 2024-2026 LittleOrange666 (orangeminecraft123@gmail.com)
+  -
+  - This program is free software: you can redistribute it and/or modify
+  - it under the terms of the GNU Affero General Public License as published
+  - by the Free Software Foundation, either version 3 of the License, or
+  - (at your option) any later version.
+  -
+  - This program is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU Affero General Public License for more details.
+  -->
+
 <template>
     <h1>404 Not Found</h1>
     <p>頁面 '<span v-text="$route.path"></span>' 不存在</p>
